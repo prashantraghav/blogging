2500f612aac9f825d42666275463b3ca81ef5dd0
EC2 Default User <ec2-user@ip-172-31-35-132.us-west-2.compute.internal>
2016-09-01 18:20:17 +0000

    navbar added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_action :set_title_bar
+
+  def set_title_bar
+    @title = "Hi! I'm Jinal"
+  end
 end
